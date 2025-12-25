e8d6f3838ec28d458dd9cb759409d452c75bfeda
aramasobhas <aramasobha@gmail.com>
2025-12-26 01:18:16 +0530

    2nd Commit for the Odd Number Display Program

---
+++ src/ex_01_java_bascis/Lab001_ODDNumbers.java
@@ -0,0 +1,16 @@
+package ex_01_java_bascis;
+
+public class Lab001_ODDNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 20; i++) {
+            i++;
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+            System.out.println("Arama");
+
+        }
+    }
+
+}
